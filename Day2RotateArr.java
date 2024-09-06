46f3ed165e350089d7aa905aa726e67eeba52460
Rangarju Viishhnu <123325064+Viishhnur@users.noreply.github.com>
2024-09-06 12:57:26 +0530

    Create Day2RotateArr.java

---
+++ Day2RotateArr.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+public class Day2RotateArr{
+    static void reverse(int[] arr,int left,int right){
+        while(left <= right){
+            int temp = arr[left];
+            arr[left] = arr[right];
+            arr[right] = temp;
+            left++;
+            right--;
+        }
+        return;
+        
+    }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+            arr[i] = sc.nextInt();
+        }
+        
+        int k = sc.nextInt();
+        k = k % n;
+        reverse(arr,0,n-1);
+        reverse(arr,0,k-1);
+        reverse(arr,k,n-1);
+
+        for(int num : arr){
+            System.out.print(num+" ");
+        }
+        sc.close();
+
+    
+    }
+}
